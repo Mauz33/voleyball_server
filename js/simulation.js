10757c5ba74e8febc71d9eb3e09bfd3f931f20c0
Maxim Eshenko <eshenko.maxim@gmail.com>
2024-07-24 14:30:57 +0300

    Перешел на отрисовку с помощью Matter js

--- js/simulation.js
+++ js/simulation.js
@@ -2,116 +2,100 @@ const Matter = require('matter-js');
 const {SERVER_WIDTH, SERVER_HEIGHT} = require("./config");
 const {server} = require("./app");
 
-const { Engine, Bodies, World } = Matter;
+const { Engine, Bodies, World, Body, Events } = Matter;
 
 const engine = Engine.create();
 const world = engine.world;
 
-engine.world.gravity.y = 3
+engine.world.gravity.y = 1;
 
-// Создаем тела для симуляции
-const groundHeight = 200
-const wallWidth = 100
+const groundHeight = 200;
+const wallWidth = 100;
 
-const ground = Bodies.rectangle(SERVER_WIDTH/2, SERVER_HEIGHT - groundHeight / 2, SERVER_WIDTH, groundHeight, { isStatic: true, restitution: 0 });
-const leftWall = Bodies.rectangle(0 + wallWidth/2, SERVER_HEIGHT/2, wallWidth, SERVER_HEIGHT, { isStatic: true, restitution: 0 });
-const rightWall = Bodies.rectangle(SERVER_WIDTH - wallWidth/2, SERVER_HEIGHT/2, wallWidth, SERVER_HEIGHT, { isStatic: true, restitution: 0 });
-const roof = Bodies.rectangle(SERVER_WIDTH/2, 0 + wallWidth / 2, SERVER_WIDTH, wallWidth, { isStatic: true, restitution: 0 });
+const wallOptions = { isStatic: true, restitution: 0.2, friction: 0.1 };
+const ground = Bodies.rectangle(SERVER_WIDTH / 2, SERVER_HEIGHT - groundHeight / 2, SERVER_WIDTH, groundHeight, wallOptions);
+ground.options = { width: SERVER_WIDTH, height: groundHeight, ...wallOptions };
 
-const playerHeight = 400
-const playerWidth = 400
+const leftWall = Bodies.rectangle(0 + wallWidth / 2, SERVER_HEIGHT / 2, wallWidth, SERVER_HEIGHT, wallOptions);
+leftWall.options = { width: wallWidth, height: SERVER_HEIGHT, ...wallOptions };
 
-const player = Bodies.rectangle(SERVER_WIDTH/2, 500, playerWidth, playerHeight, {
+const rightWall = Bodies.rectangle(SERVER_WIDTH - wallWidth / 2, SERVER_HEIGHT / 2, wallWidth, SERVER_HEIGHT, wallOptions);
+rightWall.options = { width: wallWidth, height: SERVER_HEIGHT, ...wallOptions };
+
+const roof = Bodies.rectangle(SERVER_WIDTH / 2, 0 + wallWidth / 2, SERVER_WIDTH, wallWidth, wallOptions);
+roof.options = { width: SERVER_WIDTH, height: wallWidth, ...wallOptions };
+
+const playerOptions = {
     mass: 80,
     inertia: Infinity,
-    friction: 0.0001, // Низкое значение сопротивления поверхности
-    frictionAir: 0.001 // Низкое значение сопротивления воздуха
-});
+    friction: 0,
+    frictionAir: 0.005,
+    restitution: 0
+};
 
-const ballRadius = 100
-const ball = Bodies.circle(SERVER_WIDTH/2, 100, ballRadius, {
-    restitution: 0.8,
-    mass: 0.05,
-});
+const player = Bodies.polygon(SERVER_WIDTH / 2, SERVER_HEIGHT / 2, 6, 160, playerOptions);
+player.options = {sides: 6, radius:160, ...playerOptions };
+// Поворачиваем шестиугольник на 90 градусов (π/2 радиан)
+// Body.rotate(player, Math.PI / 2);
+
+const ballOptions = {
+    restitution: 0.7,
+    mass: 5,
+    friction: 0.0001, // Увеличиваем трение для лучшего взаимодействия
+    frictionAir: 0.008
+};
+const ball = Bodies.circle(SERVER_WIDTH / 2, 100, 150, ballOptions);
+ball.options = { radius: 150, ...ballOptions };
 
 World.add(world, [ground, roof, leftWall, rightWall, ball, player]);
 
+// // Обработка столкновений
+// Events.on(engine, 'collisionStart', (event) => {
+//     const pairs = event.pairs;
+//     for (let i = 0; i < pairs.length; i++) {
+//         const pair = pairs[i];
+//         if ((pair.bodyA === player && pair.bodyB === ball) || (pair.bodyA === ball && pair.bodyB === player)) {
+//             // Если игрок находится на мяче, обнуляем вертикальную скорость игрока и мяча
+//             if (player.position.y < ball.position.y) {
+//                 Body.setVelocity(player, { x: player.velocity.x, y: 0 });
+//                 Body.setVelocity(ball, { x: ball.velocity.x, y: 0 });
+//             }
+//         }
+//     }
+// });
+
 // Обновление состояния физической симуляции
 function updateSimulation() {
+    // const maxSpeed = 10;
+    // [player, ball].forEach(body => {
+    //     if (Math.abs(body.velocity.x) > maxSpeed) {
+    //         Body.setVelocity(body, { x: Math.sign(body.velocity.x) * maxSpeed, y: body.velocity.y });
+    //     }
+    //     if (Math.abs(body.velocity.y) > maxSpeed) {
+    //         Body.setVelocity(body, { x: body.velocity.x, y: Math.sign(body.velocity.y) * maxSpeed });
+    //     }
+    // });
+
     Engine.update(engine, 1000 / 60); // Обновляем каждую 1/60 секунды
+
 }
 
 function getSimulationState() {
-    return {
-        objects: [
-            // {
-            //     id: ball.id,
-            //     position: {
-            //         x: ball.position.x,
-            //         y: ball.position.y,
-            //     },
-            //     type: 'circle',
-            //
-            //     radius: ballRadius
-            // },
-            {
-                id: ground.id,
-                type: 'rectangle',
-                position: {
-                    x: ground.position.x,
-                    y: ground.position.y,
-                },
-                width: SERVER_WIDTH,
-                height: groundHeight,
-                isStatic: true
-            },
-            // {
-            //     id: leftWall.id,
-            //     type: 'rectangle',
-            //     position: {
-            //         x: leftWall.position.x,
-            //         y: leftWall.position.y,
-            //     },
-            //     width: wallWidth,
-            //     height: SERVER_HEIGHT
-            // },
-            // {
-            //     id: rightWall.id,
-            //     type: 'rectangle',
-            //     position: {
-            //         x: rightWall.position.x,
-            //         y: rightWall.position.y,
-            //     },
-            //     width: wallWidth,
-            //     height: SERVER_HEIGHT
-            // },
-            // {
-            //     id: roof.id,
-            //     type: 'rectangle',
-            //     position: {
-            //         x: roof.position.x,
-            //         y: roof.position.y,
-            //     },
-            //     width: SERVER_WIDTH,
-            //     height: wallWidth
-            // },
-            {
-                id: player.id,
-                type: 'rectangle',
-                position: {
-                    x: player.position.x,
-                    y: player.position.y,
-                },
-                width: playerWidth,
-                height: playerHeight
-            }
-        ]
-    };
+    console.log(player.label);
+    const bodies = engine.world.bodies.map(body => ({
+        id: body.id,
+        type: body.label,
+        position: body.position,
+        angle: body.angle,
+        options: body.options
+    }));
+
+    return bodies;
 }
 
 function applyForce(action){
     const moveSpeed = 10;
-    const jumpSpeed = 30;
+    const jumpSpeed = 20;
     switch (action) {
         case 'jump':
             // Matter.Body.applyForce(player, player.position, {x:0, y: -forceMagnitude})
--- public/js/ws-client.js
+++ public/js/ws-client.js
@@ -3,9 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const socket = new WebSocket('ws://localhost:3000');
     const canvas = document.getElementById('gameCanvas');
 
-    const { Render, World, Bodies, Engine, Body } = Matter;
-// Создаем движок
-    const engine = Engine.create();
+    const { Render, Bodies, Body, Composite, Engine } = Matter;
+    const engine = Engine.create({ enableSleeping: false });
+    engine.world.gravity.y = 0;
+
 // Создаем рендерер и привязываем его к <canvas>
     const render = Render.create({
         canvas: canvas,
@@ -20,9 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Запускаем  рендер
     Render.run(render);
-
-
-
 
 // Обработчик открытия соединения
     socket.addEventListener('open', () => {
@@ -47,36 +45,39 @@ document.addEventListener('DOMContentLoaded', () => {
     const objects = {};
 // Создаем объекты на клиенте
     function createObject(data) {
-        const { id, position, type, width, height, isStatic } = data;
-        if (type === 'rectangle'){
-            console.log(scaleX)
-            console.log(scaleY)
+        const { id, position, type, options } = data;
+        if (type === 'Rectangle Body'){
             // console.log(`Создаем прямоугольник: x=${position.x * scaleX}, y=${position.y * scaleY}, width=${width * scaleX}, height=${height * scaleY}`);
-            let body = Bodies.rectangle(position.x * scaleX, position.y * scaleY, width * scaleX, height * scaleY, { isStatic: isStatic === true ? true : false});
+            let body = Bodies.rectangle(position.x * scaleX, position.y * scaleY, options.width * scaleX, options.height * scaleY, options);
             objects[id] = body;
-            World.add(engine.world, body);
+            Composite.add(render.engine.world, body);
+        }
+        else if(type === 'Circle Body'){
+            let body = Bodies.circle(position.x * scaleX, position.y * scaleY, options.radius * scaleX, options);
+            objects[id] = body;
+            Composite.add(render.engine.world, body);
+        }
+        if (type === 'Polygon Body') {
+            let body = Bodies.polygon(position.x * scaleX, position.y * scaleY, options.sides, options.radius * scaleX, options);
+            objects[id] = body;
+            Composite.add(render.engine.world, body);
         }
     }
 // Обновляем объекты на клиенте
     function updateObject(data) {
-        const { id, position, type, width, height} = data;
-        if (type === 'rectangle'){
-            // console.log(`Обновляем прямоугольник: x=${position.x * scaleX}, y=${position.y * scaleY}, width=${width * scaleX}, height=${height * scaleY}`);
-            const body = objects[id];
-            const posConverted = {
-                x: position.x * scaleX,
-                y: position.y * scaleY
-            }
-            Body.setPosition(body, posConverted);
-
-            // console.log(posConverted)
-
+        const { id, position, angle} = data;
+        const body = objects[id];
+        const posConverted = {
+            x: position.x * scaleX,
+            y: position.y * scaleY
         }
+        Body.setPosition(body, posConverted);
+        Body.setAngle(body, angle);
 
     }
     function drawSimulation(state) {
-        state.objects.forEach(obj => {
-            if(!objects[obj.id]){
+        state.forEach(obj => {
+            if (!objects[obj.id]) {
                 createObject(obj);
             } else {
                 updateObject(obj);
